7474de5aa1ee44435233eb83fef4809313b11351
Khalid OUJJA <oujja.khalid@gmail.com>
2022-06-24 23:43:01 +0200

    Fix the password issue

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://KhalidOUJJA:tinejdad00ENS@cluster0.b6d6rci.mongodb.net/blogDB", {useNewUrlParser : true});
+mongoose.connect("mongodb+srv://KhalidOUJJA:<password>@cluster0.b6d6rci.mongodb.net/blogDB", {useNewUrlParser : true});
 
 const postSchema = {
   title : String,
